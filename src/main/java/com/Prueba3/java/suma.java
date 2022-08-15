dbceed9a186c390b252d7d2559dd290bee0aa213
Santiago <jefferson02830@gmail.com>
2022-08-15 10:15:52 -0500

    conexion por terminal proyecto pru2

--- src/main/java/com/Prueba3/java/suma.java
+++ src/main/java/com/Prueba3/java/suma.java
@@ -2,6 +2,8 @@ package com.Prueba3.java;
 
 public class suma {
     public static void main(String[] args) {
+
         System.out.println("clase suma de la rama facturacionEle");
+        System.out.println("nuevo codigo despues de unir factEle y master");
     }
 }
