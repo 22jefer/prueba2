15f09204ad72571b1110455c3a71905816fe5188
Santiago <jefferson02830@gmail.com>
2022-08-14 22:34:15 -0500

    codigo de la primera rama

---
+++ src/main/java/com/Prueba3/java/operaciones.java
@@ -0,0 +1,12 @@
+package com.Prueba3.java;
+
+public class operaciones {
+
+    public static void main(String[] args) {
+        System.out.println("segunda clase");
+        System.out.println("codigo de la 1ra rama ");
+
+    }
+
+
+}
--- src/main/java/com/Prueba3/java/pruebaDos.java
+++ src/main/java/com/Prueba3/java/pruebaDos.java
@@ -1,3 +1,5 @@
+package com.Prueba3.java;
+
 public class pruebaDos {
     public static void main(String[] args) {
         System.out.println("primer codigo de prueba 2");
