318a388c8ecca8e058dc9e0343bc427523d4ac65
Santiago <jefferson02830@gmail.com>
2022-08-14 22:34:15 -0500

    clase 2 conexion de intellinj a github

---
+++ src/main/java/com/Prueba3/java/operaciones.java
@@ -0,0 +1,9 @@
+package com.Prueba3.java;
+
+public class operaciones {
+
+    public void comandos(){
+        System.out.println("segunda clase");
+        System.out.println("codigo de la 1ra rama:  #%/_&$");
+    }
+}
