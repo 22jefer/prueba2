b04f7b5b7c216f453eb1217fedb7984495a9b1f8
Santiago <jefferson02830@gmail.com>
2022-08-14 22:34:15 -0500

    codigo de la primera rama en prueba dos

--- src/main/java/com/Prueba3/java/pruebaDos.java
+++ src/main/java/com/Prueba3/java/pruebaDos.java
@@ -1,6 +1,9 @@
+package com.Prueba3.java;
+
 public class pruebaDos {
     public static void main(String[] args) {
         System.out.println("primer codigo de prueba 2");
         System.out.println("primera actualizacion");
+        System.out.println("codigo de la primera rama");
     }
 }
