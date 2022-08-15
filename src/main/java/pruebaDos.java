8ccbe314ff82e144fae636ed0bbbb0664e51d851
Santiago <jefferson02830@gmail.com>
2022-08-14 22:22:41 -0500

    segunda conexion de intellinj a github

--- src/main/java/pruebaDos.java
+++ src/main/java/pruebaDos.java
@@ -1,5 +1,6 @@
 public class pruebaDos {
     public static void main(String[] args) {
         System.out.println("primer codigo de prueba 2");
+        System.out.println("primera actualizacion");
     }
 }
